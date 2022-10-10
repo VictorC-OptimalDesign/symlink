0bdaea035c426596cd03f674685f2ad434ea8adf
victorc <victorc@optimaldesignco.com>
2022-10-10 15:28:52 -0500

    Added dummy files.

---
+++ Source/src/main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+void doStuff(void)
+{
+	printf("Do some stuff.");
+}
+
+
+int main(void)
+{
+	return 0;
+}
---
+++ Source/src/main.h
@@ -0,0 +1,10 @@
+#include <stdbool.h>
+#include <stdint.h>
+#include <string.h>
+
+#ifndef __MAIN_H
+#define __MAIN_H
+
+void doStuff(void);
+
+#endif // __MAIN_H
